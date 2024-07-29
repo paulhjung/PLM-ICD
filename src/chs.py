11aa198511a6340332ef008e449b31f8d9fbd239
Paul Jung <77036710+paulhjung@users.noreply.github.com>
2024-07-28 22:50:35 -0400

    update

--- src/chs.py
+++ src/chs.py
@@ -10,6 +10,7 @@ from torch.utils.data.dataloader import DataLoader
 from tqdm.auto import tqdm
 import transformers
 import torch
+import code
 import numpy as np
 from accelerate import Accelerator, DistributedDataParallelKwargs
 from transformers import (
@@ -34,29 +35,15 @@ logging.basicConfig(
 datasets.utils.logging.set_verbosity_warning()
 transformers.utils.logging.set_verbosity_error() #changed from info which outputs the config.json
 
-MODEL_DICT = {'roberta': RobertaForMultilabelClassification}
+##### Args
+args = parse_args()
 
 def main():
-    ## Args
-    args = parse_args()
-
-    ## Accelerator
-    # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
-    ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
-    accelerator = Accelerator(kwargs_handlers=[ddp_kwargs])
-    # Make one log on every process with the configuration for debugging.
-    # Setup logging, we only want one process per machine to log things on the screen.
-    # accelerator.is_local_main_process is only True for one process per machine.
-    logger.info(accelerator.state)
-    logger.setLevel(logging.INFO if accelerator.is_local_main_process else logging.ERROR)
-    if not accelerator.is_local_main_process:
-        logger.info("Accelerator NOT on")
-
     # If passed along, set the training seed now.
     if args.seed is not None:
         set_seed(args.seed)
 
-    ## Load datasets
+    ##### Load datasets
     # For CSV/JSON files, this script will use as labels the column called 'label' and as pair of sentences
     # the sentences in columns called 'sentence1' and 'sentence2' if such column exists or the first two columns not named
     # label if at least two columns are provided.
@@ -71,90 +58,53 @@ def main():
         data_files["validation"] = args.validation_file
     datafile_path = (args.train_file if args.train_file is not None else args.validation_file).split(".")[-1]
     raw_datasets = load_dataset(datafile_path, data_files=data_files) #data_files (str or Sequence or Mapping, optional) — Path(s) to source data file(s).
-    # More on loading datasets: https://huggingface.co/docs/datasets/loading_datasets.html.
+    # More on loading datasets: https://huggingface.co/docs/datasets/v2.20.0/en/package_reference/loading_methods#datasets.load_dataset
 
-    ## Labels
+    ##### Load labels
     # A useful fast method: https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.unique
     labels = set()
     all_codes_file = "../data/mimic3/ALL_CODES.txt" if not args.code_50 else "../data/mimic3/ALL_CODES_50.txt"
     ### Need to update the above file for ICD-10
-    if args.code_file is not None:
-        all_codes_file = args.code_file
 
     with open(all_codes_file, "r") as f:
         for line in f:
             if line.strip() != "":
                 labels.add(line.strip())
     label_list = sorted(list(labels))
+    label_to_id = {v: i for i, v in enumerate(label_list)}
     num_labels = len(label_list)
 
-    ## Load pretrained model and tokenizer
+    ##### Load pretrained model and tokenizer
     # In distributed training, the .from_pretrained methods guarantee that only one local process can concurrently download model & vocab.
     ### This next line outputs the Model config when verbosity is set to "info" level
     ### default model_name_or_path is "../models/roberta-mimic3-50", need to change this for ICD-10
     config = AutoConfig.from_pretrained(args.model_name_or_path, num_labels=num_labels, finetuning_task=args.task_name)
-    if args.model_type == "roberta":
-        config.model_mode = args.model_mode ### default is "laat"
-    tokenizer = AutoTokenizer.from_pretrained( #I am here
-        args.model_name_or_path,
-        use_fast=not args.use_slow_tokenizer,
-        do_lower_case=not args.cased)
-    model_class = MODEL_DICT[args.model_type]
+    config.model_mode = args.model_mode ### default is "laat", args.model_type = "roberta"
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path,use_fast=not args.use_slow_tokenizer,do_lower_case=not args.cased)
+    model_class = RobertaForMultilabelClassification
+    ### This is where model is trained or loaded
     if args.num_train_epochs > 0:
-        model = model_class.from_pretrained(
-            args.model_name_or_path,
-            from_tf=bool(".ckpt" in args.model_name_or_path),
-            config=config,
-        )
+        model = model_class.from_pretrained(args.model_name_or_path,from_tf=bool(".ckpt" in args.model_name_or_path),config=config)
     else:
-        model = model_class.from_pretrained( ### This is where a trained model is loaded
-            args.output_dir,
-            config=config,
-        )
+        model = model_class.from_pretrained(args.output_dir, config=config)
 
-    sentence1_key, sentence2_key = "TEXT", None
-
-    label_to_id = {v: i for i, v in enumerate(label_list)}
-    
-    padding = False
-
-    def preprocess_function(examples): #i am here
-        # Tokenize the texts
-        texts = (
-            (examples[sentence1_key],) if sentence2_key is None else (examples[sentence1_key], examples[sentence2_key])
-        )
-        result = tokenizer(*texts, padding=padding, max_length=args.max_length, truncation=True, add_special_tokens="cls" not in args.model_mode)
+    ##### Tokenize the texts
+    def preprocess_function(examples):
+        result = tokenizer(examples["TEXT"], padding=False, max_length=args.max_length, truncation=True, add_special_tokens=True)
         if "LABELS" in examples:
             result["labels"] = examples["LABELS"]
             result["label_ids"] = [[label_to_id[label.strip()] for label in labels.strip().split(';') if label.strip() != ""] if labels is not None else [] for labels in examples["LABELS"]]
         return result
-
     remove_columns = raw_datasets["train"].column_names if args.train_file is not None else raw_datasets["validation"].column_names
-    processed_datasets = raw_datasets.map(
-        preprocess_function, batched=True, remove_columns=remove_columns
-    )
-
+    processed_datasets = raw_datasets.map(preprocess_function, batched=True, remove_columns=remove_columns)
     eval_dataset = processed_datasets["validation"]
-    #code.interact(local=locals())
-    if args.num_train_epochs > 0:
-        train_dataset = processed_datasets["train"]
-        # Log a few random samples from the training set:
-        for index in random.sample(range(len(train_dataset)), 3):
-            logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
-            logger.info(f"Original tokens: {tokenizer.decode(train_dataset[index]['input_ids'])}")
+    train_dataset = processed_datasets["train"]
+    ### https://huggingface.co/docs/datasets/en/process
+    ### for testing: code.interact(local=locals())
 
+    ##### Collate data
     def data_collator(features):
         batch = dict()
-
-        if "cls" in args.model_mode:
-            for f in features:
-                new_input_ids = []
-                for i in range(0, len(f["input_ids"]), args.chunk_size - 2):
-                    new_input_ids.extend([tokenizer.cls_token_id] + f["input_ids"][i:i+(args.chunk_size)-2] + [tokenizer.sep_token_id])
-                f["input_ids"] = new_input_ids
-                f["attention_mask"] = [1] * len(f["input_ids"])
-                f["token_type_ids"] = [0] * len(f["input_ids"])
-
         max_length = max([len(f["input_ids"]) for f in features])
         if max_length % args.chunk_size != 0:
             max_length = max_length - (max_length % args.chunk_size) + args.chunk_size
@@ -179,14 +129,9 @@ def main():
                 label_ids[i, label] = 1
         batch["labels"] = label_ids
         return batch
-
-    if args.num_train_epochs > 0:
-        train_dataloader = DataLoader(
-            train_dataset, shuffle=True, collate_fn=data_collator, batch_size=args.per_device_train_batch_size
-        )
     eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=args.per_device_eval_batch_size) #default batch size is 8
-    #code.interact(local=locals())
-    # Optimizer
+    
+    ##### Optimizer
     # Split weights in two groups, one with weight decay and the other not.
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
@@ -201,11 +146,28 @@ def main():
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
 
-    # Prepare everything with our `accelerator`.
-    model, optimizer, eval_dataloader = accelerator.prepare(
-        model, optimizer, eval_dataloader
-    )
+    ##### Accelerator
+    # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
+    accelerator = Accelerator(kwargs_handlers=[DistributedDataParallelKwargs(find_unused_parameters=True)])
+    # Make one log on every process with the configuration for debugging.
+    # Setup logging, we only want one process per machine to log things on the screen.
+    # accelerator.is_local_main_process is only True for one process per machine.
+    logger.info(accelerator.state)
+    logger.setLevel(logging.INFO if accelerator.is_local_main_process else logging.ERROR)
+    if not accelerator.is_local_main_process:
+        logger.info("Accelerator NOT on")
+    model, optimizer, eval_dataloader = accelerator.prepare(model, optimizer, eval_dataloader)
+    
+    ##### Train!
+    # Get the metric function
+    if args.task_name is not None:
+        metric = load_metric("glue", args.task_name)
     if args.num_train_epochs > 0:
+        # Log a few random samples from the training set:
+        for index in random.sample(range(len(train_dataset)), 3):
+            logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
+            logger.info(f"Original tokens: {tokenizer.decode(train_dataset[index]['input_ids'])}")
+        train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=data_collator, batch_size=args.per_device_train_batch_size)
         train_dataloader = accelerator.prepare(train_dataloader)
 
         # Note -> the training dataloader needs to be prepared before we grab his length below (cause its length will be
@@ -224,13 +186,6 @@ def main():
             num_warmup_steps=args.num_warmup_steps,
             num_training_steps=args.max_train_steps,
         )
-
-    # Get the metric function
-    if args.task_name is not None:
-        metric = load_metric("glue", args.task_name)
-
-    if args.num_train_epochs > 0:
-        # Train!
         total_batch_size = args.per_device_train_batch_size * accelerator.num_processes * args.gradient_accumulation_steps
 
         logger.info("***** Running training *****")
@@ -284,6 +239,7 @@ def main():
             logger.info(f"epoch {epoch} finished")
             logger.info(f"metrics: {metrics}")
     
+    ##### Test!
     if args.num_train_epochs == 0 and accelerator.is_local_main_process:
         model.eval()
         #all_preds = []
@@ -317,7 +273,6 @@ def main():
         accelerator.wait_for_everyone()
         unwrapped_model = accelerator.unwrap_model(model)
         unwrapped_model.save_pretrained(args.output_dir, save_function=accelerator.save)
-
 
 if __name__ == "__main__":
     main()
