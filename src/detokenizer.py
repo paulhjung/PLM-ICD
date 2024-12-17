ed2a7b7a3faae0f6f8204ff9fdc2e03e4cf87710
Paul Jung <77036710+paulhjung@users.noreply.github.com>
2024-12-17 12:09:35 -0500

    minor

---
+++ src/detokenizer.py
@@ -0,0 +1,16 @@
+import transformers
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained('roberta-large', do_lower_case=True)
+
+desired_output = []
+for word_id in encoded.word_ids():
+    if word_id is not None:
+        start, end = encoded.word_to_tokens(word_id)
+        if start == end - 1:
+            tokens = [start]
+        else:
+            tokens = [start, end-1]
+        if len(desired_output) == 0 or desired_output[-1] != tokens:
+            desired_output.append(tokens)
+desired_output
